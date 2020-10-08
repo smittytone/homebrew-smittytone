5bb2ab20f470fcdfcc3d71b89bc61b7c1a2aae21
Tony Smith <smittytone@icloud.com>
2020-10-08 13:57:09 +0100

    Add The Valley

---
+++ Casks/the-valley.rb
@@ -0,0 +1,11 @@
+cask "the-valley" do
+  version "1.0.8"
+  sha256 "a13adf4597d529fa758030d9b57f0ddb7831a108b3ab596b2e25a8e9f17005c1"
+
+  url "https://www.smittytone.net/files/valley/the_valley_1_0_8.dmg"
+  name "The Valley"
+  desc "A macOS port of a 1982 real-time RPG"
+  homepage "https://www.smittytone.net/valley/index.html"
+
+  app "The Valley.app"
+end
