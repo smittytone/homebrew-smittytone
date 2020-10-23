c143b4f4222ba94cbfdcbf1d8f0a95d98b8d8a5e
smittytone <smittytone@icloud.com>
2020-10-23 18:19:43 +0100

    Latest upload @ Fri 23 Oct 2020 18:19:43 BST

--- Casks/mnu.rb
+++ Casks/mnu.rb
@@ -1,11 +1,15 @@
 cask "mnu" do
   version "1.4.2"
-  sha256 "eca8518ef4141f2adcda6c147ea68106c9f5af6898f4b67f775f4075b6dc663f"
+  sha256 "52167768c6f89c5081e5f07a7468b48c0d4aa6d26de03b938b367c08626a3dce"
 
   url "https://www.smittytone.net/files/mnu/mnu_1_4_2.dmg"
   name "MNU"
   desc "A menu bar utility for macOS"
   homepage "https://www.smittytone.net/mnu/index.html"
 
-  app "mnu.app"
+  pkg "MNU.pkg"
+
+  uninstall pkgutil: [
+    "com.bps.MNU.pkg"
+  ]
 end
