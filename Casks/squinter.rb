3b25efc3eb59fad6445d8c21049f071275d83cdd
Tony Smith <smittytone@icloud.com>
2020-11-12 16:20:37 +0000

    Release Squinter 2.4.135

--- Casks/squinter.rb
+++ Casks/squinter.rb
@@ -1,10 +1,10 @@
 cask "squinter" do
-  version "2.4.134"
-  sha256 "a117e13d906248021ac70ba707bf49df3f362c213626a36096e6fd5ad2c9e142"
+  version "2.4.135"
+  sha256 "1a16b539d03955ccfdc358390932b880fb5c3b5a88d6ac0c7a1c6037ff614aef"
 
-  url "https://www.smittytone.net/files/squinter/squinter_2_4_134.dmg"
+  url "https://www.smittytone.net/files/squinter/squinter_2_4_135.dmg"
   name "Squinter"
-  desc "A development tool to manage app and library files for Electric Imp projects"
+  desc "A development tool to manage app and library source code files for Electric Imp projects"
   homepage "https://www.smittytone.net/squinter/index.html"
 
   app "Squinter.app"
