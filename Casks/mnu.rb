b2081cfa355392810bdbb0baaa704d64c095208b
smittytone <smittytone@icloud.com>
2020-11-13 20:50:48 +0000

    Latest upload @ Fri 13 Nov 2020 20:50:48 GMT

--- Casks/mnu.rb
+++ Casks/mnu.rb
@@ -2,7 +2,7 @@ cask "mnu" do
   version "1.4.2"
   sha256 "607cb4fe5ce4f13f6411f45403c96c70f1855b71122d94da4e4d8a4cbb9b8024"
 
-  url "https://www.smittytone.net/files/mnu/mnu_1_4_2.dmg"
+  url "https://smittytone.net/files/mnu/mnu_1_4_2.dmg"
   name "MNU"
   desc "A menu bar utility for macOS"
   homepage "https://www.smittytone.net/mnu/index.html"
--- Casks/squinter.rb
+++ Casks/squinter.rb
@@ -2,7 +2,7 @@ cask "squinter" do
   version "2.4.135"
   sha256 "1a16b539d03955ccfdc358390932b880fb5c3b5a88d6ac0c7a1c6037ff614aef"
 
-  url "https://www.smittytone.net/files/squinter/squinter_2_4_135.dmg"
+  url "https://smittytone.net/files/squinter/squinter_2_4_135.dmg"
   name "Squinter"
   desc "A development tool to manage app and library source code files for Electric Imp projects"
   homepage "https://www.smittytone.net/squinter/index.html"
