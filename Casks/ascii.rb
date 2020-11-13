60f2f487cd5968e9e68c853a2352858bb8a8891b
smittytone <smittytone@icloud.com>
2020-11-13 20:49:00 +0000

    Latest upload @ Fri 13 Nov 2020 20:49:00 GMT

--- Casks/ascii.rb
+++ Casks/ascii.rb
@@ -2,7 +2,7 @@ cask "ascii" do
   version "1.3.0"
   sha256 "7d82bda1871c0b01e9520585fa3c179e0356eeb819256c1439aa540839a7fab5"
 
-  url "https://www.smittytone.net/files/ascii/ascii_1_3_0.dmg"
+  url "https://smittytone.net/files/ascii/ascii_1_3_0.dmg"
   name "Ascii"
   desc "A tool to help you create glyphs and icons for monochrome and bi-colour 8x8 LED matrix displays"
   homepage "https://www.smittytone.net/ascii/index.html"
