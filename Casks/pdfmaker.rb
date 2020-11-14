ca7863b74dc0687097131fffa3e800a89692a529
smittytone <smittytone@icloud.com>
2020-11-14 13:25:16 +0000

    pdfmaker test

--- Casks/pdfmaker.rb
+++ Casks/pdfmaker.rb
@@ -2,7 +2,7 @@ cask "pdfmaker" do
   version "2.2.0"
   sha256 "34dfba0b4089a3c0360ab666b573212ee6980115ab9a33a05ab0f59cb2d1963b"
 
-  url "https://smittytone.net/files/pdfmaker/pdfmaker-2-1-0.dmg"
+  url "https://smittytone.net/files/pdfmaker/pdfmaker_2_2_0.dmg"
   name "pdfmaker"
   desc "A command-line tool to build PDFs from JPEG images"
   homepage "https://www.smittytone.net/pdfmaker/index.html"
