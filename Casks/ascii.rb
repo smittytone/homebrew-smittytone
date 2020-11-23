23f76d89737616b596e0e3ba92f1aa35fff23d52
smittytone <smittytone@icloud.com>
2020-11-23 20:03:35 +0000

    Latest upload @ Mon 23 Nov 2020 20:03:35 GMT

--- Casks/ascii.rb
+++ Casks/ascii.rb
@@ -1,15 +1,11 @@
 cask "ascii" do
   version "1.3.1"
-  sha256 "64e6e143645cd07e4f64443461c5fae53911e96e5d230d696eafcee839ec1b7c"
+  sha256 "9f274d20ffac7699ade84465bd57f35f7581fa0d3799ebd8d495a8e7d424e2f3"
 
-  url "https://smittytone.net/files/ascii/ascii_1_3_1.dmg"
+  url "https://smittytone.net/files/ascii/ascii_1_3_1h.dmg"
   name "ASCII"
   desc "A tool to help you create glyphs and icons for monochrome and bi-colour 8x8 LED matrix displays"
   homepage "https://www.smittytone.net/ascii/index.html"
 
-  pkg "ASCII.pkg"
-
-  uninstall pkgutil: [
-    "com.bps.ASCII.pkg"
-  ]
+  app "ASCII.app"
 end
