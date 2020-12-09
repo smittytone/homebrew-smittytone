75f52c20248c083912201dbe9dc10184ce18f8c4
Tony Smith <smittytone@icloud.com>
2020-12-09 14:19:52 +0000

    Release MNU 1.4.6 for testing

--- Casks/mnu.rb
+++ Casks/mnu.rb
@@ -1,11 +1,11 @@
 cask "mnu" do
-  version "1.4.5"
-  sha256 "138b2ebd063c9c8eae948088338a0ba95fe13491f9c05c477fc57ef9c86e16e6"
+  version "1.4.6"
+  sha256 "5465632803a6727537118c5461e5a45182cfcf72eed5c151804f930b2a2135d2"
 
-  url "https://smittytone.net/files/mnu/mnu_1_4_5h.dmg"
+  url "https://smittytone.net/files/mnu/mnu_1_4_6h.dmg"
   name "MNU"
   desc "A menu bar utility for macOS"
-  homepage "https://www.smittytone.net/mnu/index.html"
+  homepage "https://smittytone.net/mnu/index.html"
 
   app "MNU.app"
 end
