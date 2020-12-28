ed4af13d3f58b2a8654ce2532a979e11d12404af
Tony Smith <smittytone@icloud.com>
2020-12-28 10:41:25 +0000

    Release imageprep 6.2.0

--- Casks/imageprep.rb
+++ Casks/imageprep.rb
@@ -1,13 +1,13 @@
 cask "imageprep" do
-  version "6.1.0"
-  sha256 "ef8c6351ff954762e2255d1374f404f7da4e8c71f9d14b7ce5a8a223aa3586fa"
+  version "6.2.0"
+  sha256 "64e5987d45451f99a4318ff77248157339b69edb26ccf45296b90d1b4ac156f3"
 
-  url "https://smittytone.net/files/imageprep/imageprep_6_1_0.dmg"
+  url "https://smittytone.net/files/imageprep/imageprep_6_2_0.dmg"
   name "imageprep"
   desc "A command-line tool to batch-process image files"
   homepage "https://smittytone.net/imageprep/index.html"
 
-  pkg "imageprep-6.1.0.pkg"
+  pkg "imageprep-6.2.0.pkg"
 
   uninstall pkgutil: [
     "com.bps.imageprep.pkg"
