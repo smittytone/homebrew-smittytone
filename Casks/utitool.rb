e23504ff00832bfa5a31eaf6a408a0a265519f2a
smittytone <smittytone@icloud.com>
2021-01-12 20:34:18 +0000

    Add utitools

---
+++ Casks/utitool.rb
@@ -0,0 +1,15 @@
+cask "utitool" do
+  version "1.0.0"
+  sha256 "b600674ac6b18034ea6a515edd2475a5a1f274fba9da1a8d370ee488ce2ac615"
+
+  url "https://smittytone.net/files/utitool/utitool_1_0_0.dmg"
+  name "utitool"
+  desc "A command-line tool to batch-process image files"
+  homepage "https://smittytone.net/index.html"
+
+  pkg "utitool-1.0.0.pkg"
+
+  uninstall pkgutil: [
+    "com.bps.utitool.pkg"
+  ]
+end
