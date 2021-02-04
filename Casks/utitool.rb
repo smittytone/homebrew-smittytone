a637d2f9b7c396d1d39d35006cf0bdcbbf5e4524
smittytone <smittytone@icloud.com>
2021-02-04 18:34:41 +0000

    Release utitool 1.0.1

--- Casks/utitool.rb
+++ Casks/utitool.rb
@@ -1,13 +1,13 @@
 cask "utitool" do
-  version "1.0.0"
-  sha256 "b600674ac6b18034ea6a515edd2475a5a1f274fba9da1a8d370ee488ce2ac615"
+  version "1.0.1"
+  sha256 "f6a1118315e0edf10d2f1f7aa078570233d58f5f5163931cc2a091e79b9e0f79"
 
-  url "https://smittytone.net/files/utitool/utitool_1_0_0.dmg"
+  url "https://smittytone.net/files/utitool/utitool_1_0_1.dmg"
   name "utitool"
   desc "A macOS command line tool you can use to reveal a file's Uniform Type Identifier (UTI)"
   homepage "https://smittytone.net/utitool/index.html"
 
-  pkg "utitool-1.0.0.pkg"
+  pkg "utitool-1.0.1.pkg"
 
   uninstall pkgutil: [
     "com.bps.utitool.pkg"
