e055ac683da44a0fb9bcd45ff6c06b440f0b0e7b
smittytone <smittytone@icloud.com>
2021-05-19 19:19:53 +0100

    Release MNU 1.5.2

--- Casks/mnu.rb
+++ Casks/mnu.rb
@@ -1,8 +1,8 @@
 cask "mnu" do
-  version "1.5.1"
-  sha256 "0423483b5d320be1134f02caae1edba33b99961d2f4377d77bb83b9eb1e021fe"
+  version "1.5.2"
+  sha256 "fde05a3a5920fadb016e00cd2c6c6f59a91c7fbcc035894e7a4d8a582d6c41d5"
 
-  url "https://smittytone.net/files/mnu/mnu_1_5_1h.dmg"
+  url "https://smittytone.net/files/mnu/mnu_1_5_2h.dmg"
   name "MNU"
   desc "A menu bar utility for macOS"
   homepage "https://smittytone.net/mnu/index.html"
