9ecafc71f21b396e5535fc76db932f15ad9836fb
Tony Smith <smittytone@icloud.com>
2022-05-23 14:48:45 +0100

    Add manpage stanze to pdfmaker

--- Casks/pdfmaker.rb
+++ Casks/pdfmaker.rb
@@ -1,13 +1,15 @@
 cask "pdfmaker" do
-  version "2.3.1"
-  sha256 "af04e5290dba3b0c9bad67712ad2e2ce3bf8b82e3b7d3a854b748390984c875d"
+  version "2.3.2"
+  sha256 ""
 
-  url "https://smittytone.net/files/pdfmaker/pdfmaker_2_3_1.dmg"
+  url "https://smittytone.net/files/pdfmaker/pdfmaker_2_3_2.dmg"
   name "pdfmaker"
   desc "A command-line tool to build PDFs from JPEG images"
   homepage "https://www.smittytone.net/pdfmaker/index.html"
 
-  pkg "pdfmaker-2.3.1.pkg"
+  pkg "pdfmaker-2.3.2.pkg"
+
+  manpage "pdfmaker.1"
 
   uninstall pkgutil: [
     "com.bps.pdfmaker.pkg"
