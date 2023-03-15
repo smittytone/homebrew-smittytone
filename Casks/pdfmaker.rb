538ccfe5110ff46aa4fe35178a86856e2f12e078
smittytone <smitty@smittytone.net>
2023-03-15 12:23:24 +0000

    Latest upload @ Wed 15 Mar 2023 12:23:24 GMT

--- Casks/pdfmaker.rb
+++ Casks/pdfmaker.rb
@@ -1,6 +1,6 @@
 cask "pdfmaker" do
   version "2.3.4"
-  sha256 "flash_img: flash_pico
+  sha256 "36058e351d4ae71a2a171ace358e622783ea76290f4bd24b25c831e0c63b107c"
   flash_url: https://www.raspberrypi.org/products/raspberry-pi-pico/"
 
   url "https://smittytone.net/files/pdfmaker/pdfmaker_2_3_4.dmg"
