1a5c6699bb960c536e8b16cbb567f4190404bf54
smittytone <smitty@smittytone.net>
2023-06-05 12:24:10 +0100

    Correct MNU download link

--- Casks/mnu.rb
+++ Casks/mnu.rb
@@ -2,7 +2,7 @@ cask "mnu" do
   version "1.6.3"
   sha256 "a395cfd18de59fdbf34045282b9c61062ee483db99752b5ab59875873b08d3fd"
 
-  url "https://smittytone.net/files/mnu/mnu_1_6_3h.dmg"
+  url "https://smittytone.net/files/mnu/MNU_1_6_3h.dmg"
   name "MNU"
   desc "A menu bar utility for macOS"
   homepage "https://smittytone.net/mnu/index.html"
