cdc814722e27e86c1e81bda8f70153e1143426a9
smittytone <smittytone@icloud.com>
2023-07-09 11:10:21 +1000

    pdfmaker 2.3.5

--- Casks/pdfmaker.rb
+++ Casks/pdfmaker.rb
@@ -1,13 +1,13 @@
 cask "pdfmaker" do
-  version "2.3.4"
-  sha256 "36058e351d4ae71a2a171ace358e622783ea76290f4bd24b25c831e0c63b107c"
+  version "2.3.5"
+  sha256 "bc63214939c7d916e3b3800c7c5a1ca16d4c127d587c887dc2747194522ff447"
 
-  url "https://smittytone.net/files/pdfmaker/pdfmaker_2_3_4.dmg"
+  url "https://smittytone.net/files/pdfmaker/pdfmaker_2_3_5.dmg"
   name "pdfmaker"
   desc "A command-line tool to build PDFs from JPEG images"
   homepage "https://www.smittytone.net/pdfmaker/index.html"
 
-  pkg "pdfmaker-2.3.4.pkg"
+  pkg "pdfmaker-2.3.5.pkg"
 
   manpage "pdfmaker.1"
 
