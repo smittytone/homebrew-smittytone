355f32291510a675179c0ea94c99a114e9e2be92
smittytone <smittytone@icloud.com>
2023-07-09 11:20:18 +1000

    Latest upload @ Sun  9 Jul 2023 11:20:18 AEST

--- Casks/pdfmaker.rb
+++ Casks/pdfmaker.rb
@@ -1,6 +1,6 @@
 cask "pdfmaker" do
   version "2.3.5"
-  sha256 "bc63214939c7d916e3b3800c7c5a1ca16d4c127d587c887dc2747194522ff447"
+  sha256 "1d1630f10561a1e14ba92bbc3bcd5b28189334abbd1b8d8370913623860b6195"
 
   url "https://smittytone.net/files/pdfmaker/pdfmaker_2_3_5.dmg"
   name "pdfmaker"
