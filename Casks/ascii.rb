c02fe0163819112b1d157ba7d6ae4ab8db039d34
Tony Smith (smittytone) <tony@smittytone.net>
2025-03-24 12:49:38 +0000

    Add Ascii 2.0.0

--- Casks/ascii.rb
+++ Casks/ascii.rb
@@ -1,8 +1,8 @@
 cask "ascii" do
-  version "1.4.0"
-  sha256 "a9827f285c7c65211d27c388c60cf00f066e4c93c579d13227ceef5901043b55"
+  version "2.0.0"
+  sha256 "3e205baf427c5d407d4295ebd153ca9073fce29f73b110a7ae43a9c6ee5c58a4"
 
-  url "https://smittytone.net/files/ascii/ascii_1_4_0.dmg"
+  url "https://smittytone.net/files/ascii/ascii_2_0_0.dmg"
   name "ASCII"
   desc "A tool to help you create glyphs and icons for monochrome and bi-colour 8x8 LED matrix displays"
   homepage "https://smittytone.net/ascii/index.html"
