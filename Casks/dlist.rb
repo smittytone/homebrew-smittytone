f78b49a7d9a7542a41338d9032f44a83f2110929
Tony Smith (smittytone) <tony@smittytone.net>
2025-08-22 12:24:57 +0100

    URL tweak

--- Casks/dlist.rb
+++ Casks/dlist.rb
@@ -2,7 +2,7 @@ cask "dlist" do
   version "0.2.0"
   sha256 "5ac1936a51a7318ec3fa92d8e1e4c58e28cccdc2a1876d15cb38430b2d8bb118"
 
-  url "https://smittytone.net/files/dlist/dlist_0_2_0.dmg"
+  url "https://www.smittytone.net/files/dlist/dlist_0_2_0.dmg"
   name "dlist"
   desc "A macOS command line tool you can use to reveal a file's Uniform Type Identifier (UTI)"
   homepage "https://smittytone.net/index.html"
