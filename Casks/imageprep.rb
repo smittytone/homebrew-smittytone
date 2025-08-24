d722c7f0cd4f79f9435b5cb7cfaf00bfe6cd42b1
Tony Smith (smittytone) <tony@smittytone.net>
2025-08-24 19:12:15 +0100

    Release imageprep 7.1.0

--- Casks/imageprep.rb
+++ Casks/imageprep.rb
@@ -1,13 +1,13 @@
 cask "imageprep" do
-  version "7.0.0"
-  sha256 "dba4eb66156a01ba18ff3bc21c85ab5b7e3c4a184a4479517339197f7a57df9f"
+  version "7.1.0"
+  sha256 "ec240870cd7675feada0319101f580c9ceac199ceb62778b88ea8cc90d4a2ca7"
 
-  url "https://www.smittytone.net/files/imageprep/imageprep_7_0_0.dmg"
+  url "https://www.smittytone.net/files/imageprep/imageprep_7_1_0.dmg"
   name "imageprep"
   desc "A command-line tool to batch-process image files"
   homepage "https://www.smittytone.net/imageprep/index.html"
 
-  pkg "imageprep-7.0.0.pkg"
+  pkg "imageprep-7.1.0.pkg"
 
   manpage "imageprep.1"
 
