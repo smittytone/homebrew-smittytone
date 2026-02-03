b2779ac1b09f905b7cfd5827f4d7bf3f9f783d0c
Tony Smith (smittytone) <tony@smittytone.net>
2026-02-03 09:42:10 +0000

    W2T 0.2.0

--- Casks/word2text.rb
+++ Casks/word2text.rb
@@ -1,13 +1,13 @@
 cask "word2text" do
-  version "0.1.5"
-  sha256 "090f362b57bbe96741fc5938e9ffc03af441b63274ad75362f512ea583f128d8"
+  version "0.2.0"
+  sha256 "e87a0c07fa89c7c7018c2ca7f6f6ba27b5468081d02a126aafd1696c855ef4d7"
 
-  url "https://www.smittytone.net/files/word2text/word2text_0_1_5.dmg"
+  url "https://www.smittytone.net/files/word2text/word2text_0_2_0.dmg"
   name "word2text"
   desc "A command-line tool to build convert Psion Word files to plain text or Markdown"
   homepage "https://www.smittytone.net/word2text/"
 
-  pkg "word2text-0.1.5.pkg"
+  pkg "word2text-0.2.0.pkg"
 
   manpage "word2text.1"
 
