8ec66ef94cc79002a4b3a9a820c7f5da70a972a2
Tony Smith (smittytone) <tony@smittytone.net>
2026-05-13 14:26:39 +1000

    Release pdfmaker 2.5.1

--- Casks/pdfmaker.rb
+++ Casks/pdfmaker.rb
@@ -1,13 +1,13 @@
 cask "pdfmaker" do
-  version "2.5.0"
-  sha256 "99051326eb57099cd9ce4539349a08758a121f18b1d2b9923fac1d89ca910f17"
+  version "2.5.1"
+  sha256 "0144a9d1e2a99e7262e30fe760cde51c2bd9f8943f9f462d2d9681c9f14bdc78"
 
-  url "https://www.smittytone.net/files/pdfmaker/pdfmaker_2_5_0.dmg"
+  url "https://www.smittytone.net/files/pdfmaker/pdfmaker_2_5_1.dmg"
   name "pdfmaker"
   desc "A command-line tool to build PDFs from JPEG, PNG, WebP, TIFF and BMP images"
   homepage "https://www.smittytone.net/pdfmaker/index.html"
 
-  pkg "pdfmaker-2.5.0.pkg"
+  pkg "pdfmaker-2.5.1.pkg"
 
   manpage "pdfmaker.1"
 
