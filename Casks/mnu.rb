8984cb5956f9785f46f2e7e504a3dd9f17d294f4
Tony Smith (smittytone) <tony@smittytone.net>
2026-05-26 14:20:57 +0100

    MNU 2.4.0

--- Casks/mnu.rb
+++ Casks/mnu.rb
@@ -1,8 +1,8 @@
 cask "mnu" do
-  version "2.3.0"
-  sha256 "cda4800ed4779c8cd5d14374e12031f069ae8b128a57af327eec36ceb61ba338"
+  version "2.4.0"
+  sha256 "ab95f006d408d98aecfe631618f5895b4e2e0693a0f4fc0e78758106dcfc5169"
 
-  url "https://www.smittytone.net/files/mnu/mnu_2_3_0h.dmg"
+  url "https://www.smittytone.net/files/mnu/mnu_2_4_0h.dmg"
   name "MNU"
   desc "A menu bar utility for macOS"
   homepage "https://www.smittytone.net/mnu/index.html"
