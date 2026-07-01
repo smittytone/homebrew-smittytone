72a32bec2d03f4fdcc213f14a8906695a7df1abb
Tony Smith (smittytone) <tony@smittytone.net>
2026-07-01 22:01:57 +0100

    Release dl 0.2.4

--- Casks/dlist.rb
+++ Casks/dlist.rb
@@ -1,13 +1,13 @@
 cask "dlist" do
-  version "0.2.0"
-  sha256 "5ac1936a51a7318ec3fa92d8e1e4c58e28cccdc2a1876d15cb38430b2d8bb118"
+  version "0.2.4"
+  sha256 "356eb91baab469f2fcd43419379a645c01ce0f46bb48de662c7dd2ba730c756e"
 
-  url "https://www.smittytone.net/files/dlist/dlist_0_2_0.dmg"
+  url "https://www.smittytone.net/files/dlist/dlist_0_2_4.dmg"
   name "dlist"
   desc "A macOS command line tool you can use to reveal a file's Uniform Type Identifier (UTI)"
   homepage "https://smittytone.net/index.html"
 
-  pkg "dlist-0.2.0.pkg"
+  pkg "dlist-0.2.4.pkg"
 
   uninstall pkgutil: [
     "com.bps.dlist.pkg"
