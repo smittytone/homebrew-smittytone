17bee19c4ce2e9aee7d997aeb528cb9cbdeec7f2
Tony Smith (smittytone) <tony@smittytone.net>
2026-08-14 09:13:10 +0100

    Tweak text

--- Casks/dlist.rb
+++ Casks/dlist.rb
@@ -4,7 +4,7 @@ cask "dlist" do
 
   url "https://www.smittytone.net/files/dlist/dlist_0_3_1.dmg"
   name "dlist"
-  desc "A macOS command line tool you can use to get connected devices’ system paths, e.g., /dev/cu.usbmodem101, and pipe them into other tool, such as minicom -D $(dlist)"
+  desc "A macOS command line tool you can use to get connected devices’ system paths, e.g., \u{001B}[4m/dev/cu.usbmodem101\u{001B}[0m, and pipe them into other tools and apps, e.g., minicom -D $(dlist)"
   homepage "https://smittytone.net/index.html"
 
   pkg "dlist-0.3.1.pkg"
