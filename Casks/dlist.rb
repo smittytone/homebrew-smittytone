b953a38ff7e9c275c8a02fa7ddacda7fbb39fb5a
Tony Smith (smittytone) <tony@smittytone.net>
2026-08-14 09:09:43 +0100

    Release dlist 0.3.1

--- Casks/dlist.rb
+++ Casks/dlist.rb
@@ -4,7 +4,7 @@ cask "dlist" do
 
   url "https://www.smittytone.net/files/dlist/dlist_0_3_1.dmg"
   name "dlist"
-  desc "A macOS command line tool you can use to reveal a file's Uniform Type Identifier (UTI)"
+  desc "A macOS command line tool you can use to get connected devices’ system paths, e.g., /dev/cu.usbmodem101, and pipe them into other tool, such as minicom -D $(dlist)"
   homepage "https://smittytone.net/index.html"
 
   pkg "dlist-0.3.1.pkg"
